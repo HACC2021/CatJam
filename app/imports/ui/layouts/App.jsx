f6823e7b07e1ca99a87bad8ae118fe7b84af771f
Wenhao Qiu <wenhaoq@hawaii.edu>
2021-11-07 17:58:13 -1000

    Update App.jsx

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -84,7 +84,7 @@ const AdminProtectedRoute = ({ component: Component, ...rest }) => (
       const isAdmin = Roles.userIsInRole(Meteor.userId(), 'admin');
       return (isLogged && isAdmin) ?
         (<Component {...props} />) :
-        (<Redirect to={{ pathname: '/signin', state: { from: props.location } }}/>
+        (<Redirect to={{ pathname: '/', state: { from: props.location } }}/>
         );
     }}
   />
