bc42bd0dd9c13e48992bf962dfd913d70ca5d2d6
Wenhao Qiu <wenhaoq@hawaii.edu>
2021-11-09 11:11:45 -1000

    added survey collection

---
+++ app/imports/api/survey/Survey.js
@@ -0,0 +1,44 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The SurveyCollection. It encapsulates state and variable values for stuff.
+ */
+class SurveyCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'SurveyCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      experience: {
+        type: String,
+        allowedValues: ['Very satisfied', 'Satisfied', 'Neither satisfied nor dissatisfied', 'dissatisfied', 'very dissatisfied'],
+      },
+      bot: {
+        type: String,
+        allowedValues: ['Very satisfied', 'Satisfied', 'Neither satisfied nor dissatisfied', 'dissatisfied', 'very dissatisfied'],
+      },
+      again: {
+        type: String,
+        allowedValues: ['Very satisfied', 'Satisfied', 'Neither satisfied nor dissatisfied', 'dissatisfied', 'very dissatisfied'],
+      },
+      humanOrBot: {
+        type: String,
+        allowedValues: ['Very satisfied', 'Satisfied', 'Neither satisfied nor dissatisfied', 'dissatisfied', 'very dissatisfied'],
+      },
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the SurveyCollection.
+ * @type {SurveyCollection}
+ */
+export const Survey = new SurveyCollection();
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { UserFeedbacks } from '../../api/userFeedback/UserFeedback';
 import { Intents } from '../../api/Intents/Intents';
+import { Survey } from '../../api/survey/Survey';
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
@@ -15,6 +16,13 @@ Meteor.publish(UserFeedbacks.adminPublicationName, function () {
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise publish nothing.
 Meteor.publish(Intents.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Intents.collection.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish(Survey.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Intents.collection.find();
   }
