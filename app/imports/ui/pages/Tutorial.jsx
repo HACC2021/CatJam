c5bb737392bdf2f3ae243512680fb62234afacc8
Breadonn <braydonn@hawaii.edu>
2021-11-08 22:52:17 -1000

    Tutorial Page finished

--- app/imports/ui/pages/Tutorial.jsx
+++ app/imports/ui/pages/Tutorial.jsx
@@ -16,11 +16,20 @@ class Tutorial extends React.Component {
         <p>This is the navigation bar. Clicking on elements in the bar allows you to move to other pages in the application.</p>
         <Header as="h3" textAlign="center">Chatbot</Header>
         <Image src='images/chatbot.png'/>
-        <p>Above is the chatbot page. This is where you will be asking the chatbot questions and receiving answers. If you have an inquiry, it can be sent to the chatbot through text or voice by typing</p>
-        <p>words into the textbox on the bottom, or clicking on the mic symbol on the bottom right of the chatbot window respectively.</p>
+        {/* eslint-disable-next-line max-len */}
+        <p>Above is the chatbot page. This is where you will be asking the chatbot questions and receiving answers. If you have an inquiry, it can be sent to the chatbot through text or voice by typing words into the text-box on the bottom, or clicking on the mic symbol on the bottom right of the chatbot window respectively.</p>
         <Image src='images/question.png'/>
         <p>After asking a question the text will be displayed inside a message on the left side of the screen. Then the chatbot will send a response contained in a message on the right side of the screen.</p>
         <Image src='images/message1.png'/>
+        <p>If you need to contact OHA for any reason, their phone and email contacts are available via the buttons below the chatbot. A link to the OHA official website is also provided.</p>
+        <Image src='img.png'/>
+        <Header as="h3" textAlign="center">Send Feedback</Header>
+        <Image src='images/feedback.png'/>
+        {/* eslint-disable-next-line max-len */}
+        <p>This is the Send Feedback page. This page allows users to send feedback regarding the chatbot or website. In order to send in feedback the user must fill out their contact details, a name for the input, select what type of feedback they are sending in, and provide a description of their feedback.</p>
+        <Image src='images/feedbackP.png'/>
+        <p>When you are done filling out all the fields press the submit button and your feedback will be recorded for staff to look at and consider.</p>
+        <Image src='images/submit.png'/>
       </Container>
     );
   }
