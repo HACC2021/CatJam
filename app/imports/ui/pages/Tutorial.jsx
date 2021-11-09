489382ec8fd223d631cce6e47fe4199f76bf4cb2
Breadonn <braydonn@hawaii.edu>
2021-11-08 22:05:32 -1000

    WIP

--- app/imports/ui/pages/Tutorial.jsx
+++ app/imports/ui/pages/Tutorial.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Image } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Tutorial extends React.Component {
@@ -8,8 +8,19 @@ class Tutorial extends React.Component {
   render() {
     return (
       <Container>
-        <Header as="h2" textAlign="center">Tutorial</Header>
-        <p>CatJAM</p>
+        <Header as="h2" textAlign="center">Chatbot User Guide</Header>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <p>Welcome to CatJAM's chatbot for the Office of Hawaiian Affairs. This is a short tutorial on how to navigate and utilize the chatbot website and application.</p>
+        <Header as="h3" textAlign="center">Navigation Bar</Header>
+        <Image src='images/Navbar.png'/>
+        <p>This is the navigation bar. Clicking on elements in the bar allows you to move to other pages in the application.</p>
+        <Header as="h3" textAlign="center">Chatbot</Header>
+        <Image src='images/chatbot.png'/>
+        <p>Above is the chatbot page. This is where you will be asking the chatbot questions and receiving answers. If you have an inquiry, it can be sent to the chatbot through text or voice by typing</p>
+        <p>words into the textbox on the bottom, or clicking on the mic symbol on the bottom right of the chatbot window respectively.</p>
+        <Image src='images/question.png'/>
+        <p>After asking a question the text will be displayed inside a message on the left side of the screen. Then the chatbot will send a response contained in a message on the right side of the screen.</p>
+        <Image src='images/message1.png'/>
       </Container>
     );
   }
